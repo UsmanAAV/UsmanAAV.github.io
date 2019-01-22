bca6496b9d77e0b7397e8c3665628999964f3e60
UsmanAAV <usman.a.a.v@gmail.com>
2019-01-22 22:32:57 +0500

    Updates

---
+++ precache-manifest.3c638f08b46fbe3357e41742e872ab6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64516b0ed624e7037a7c",
+    "url": "/static/css/main.3b08dae2.chunk.css"
+  },
+  {
+    "revision": "64516b0ed624e7037a7c",
+    "url": "/static/js/main.64516b0e.chunk.js"
+  },
+  {
+    "revision": "79ab894b48b9a9a5ba45",
+    "url": "/static/js/1.79ab894b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7760648cf346ee2a8da6db03e212377e",
+    "url": "/static/media/me_2.7760648c.jpg"
+  },
+  {
+    "revision": "61904fa9b7eb7205a4bf7b8dacf8023b",
+    "url": "/index.html"
+  }
+];
